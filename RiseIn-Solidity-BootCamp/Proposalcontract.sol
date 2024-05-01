4667797491f61edc64336b007f88fd40dd2c1119
sukanta-nandi <sukantanandi0@gmail.com>
2024-05-01 21:21:40 +0530

    proposal create function

--- RiseIn-Solidity-BootCamp/Proposalcontract.sol
+++ RiseIn-Solidity-BootCamp/Proposalcontract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 contract ProposalContract {
+    uint256 private counter; // This line is added
+
     struct Proposal {
         string description; // Description of the proposal
         uint256 approve; // Number of approve votes
@@ -13,5 +15,12 @@ contract ProposalContract {
 
     // In this mapping you have uint256 as the key and the proposal as the value. So, you will be able to get the proposal based on its key uint256 value, which will be the id of the proposal.
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+    // Calldata:  calldata keyword in Solidity function parameters to pass large, complex data types like arrays, structs and sometimes 
+    // strings more efficiently by storing them in immutable call data rather than temporary memory.
+    function create(string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+    }
 
 }
